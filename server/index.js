b80bef664d569d40b8f2058ad3b4fff47d06fdc4
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-30 23:57:59 +0530

    added server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import connectDB from "./mongodb/connect.js";
+import tasksRoutes from './routes/tasksRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', async (req, res) => {
+	res.send('Hello From OPEN AI!');
+})
+
+app.use("/tasks", tasksRoutes);
+
+const startServer = async () => {
+	const port = process.env.PORT || 5000;
+
+	try {
+        connectDB(process.env.MONGODB_URL)
+		app.listen(port, () => {
+			console.log(`Server running on port ${port}`);
+		});
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+startServer();
