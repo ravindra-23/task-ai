ef9eb3680af0933f9b3e241a8d0028ffd14148a0
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-30 14:35:28 +0530

    added logo component

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import LogoIcon from '../assets/task.png'
+
+const Logo = () => {
+  return (
+    <Link 
+        to='/'
+        className='text-brand uppercase font-jakarta font-medium text-medium flex items-center gap-2 hover:text-custom_02 transition-all duration-300'
+    >
+        <img 
+            src={LogoIcon}
+            alt="Task-ai"
+            draggable="false"
+            loading="lazy"
+            className="w-8 h-8"
+        />
+        <span className="hidden md:block">Task AI</span>
+    </Link>
+  )
+}
+
+export default Logo
