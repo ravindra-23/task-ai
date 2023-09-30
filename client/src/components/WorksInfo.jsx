a96075447e89a3150fee3afb75c5d97349f7a3c3
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-30 19:57:06 +0530

    added WorksInfo component

---
+++ client/src/components/WorksInfo.jsx
@@ -0,0 +1,24 @@
+const WorksInfo = () => {
+  return (
+    <div className='my-24'>
+        <div>
+            <h1 className="text-xlarge font-poppins mb-4 font-bold leading-[60px]">
+                See How It <span className="text-brand">Works</span>
+            </h1>
+            <p className="max-w-[600px] text-large text-secondary opacity-80">
+                Do You Need To Wash You Car? <br />Task AI Will Quickly Show You How!
+                Just Enter "Wash Car" In The Text Box.
+            </p>
+            <p className="max-w-[600px] text-custom_03 mt-2">
+                Then, click "Create" and let Task AI get to work. In seconds,
+                you&#x2019;ll receive step by step detailed instructions on how to
+                complete the task. No more getting overwhelmed at the process of doing
+                a chore or activity. Task AI gives you simple and concise steps to
+                get the job done.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default WorksInfo
