28b675354b10f537271c4e1dfbd132f1004b2bfe
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-01 14:26:50 +0530

    added tasks component

---
+++ client/src/scenes/pages/Tasks.jsx
@@ -0,0 +1,37 @@
+import { CreateTaskSection, CreatedTaskCard, TasksList } from "../../components"
+import { useState, useEffect } from "react";
+
+
+const Tasks = () => {
+  const [createdTask, setCreatedTask] = useState(null);
+  const [created, setCreated] = useState(false);
+  const [reFetching, setRefetching] = useState(false);
+  console.log('Created Task:', createdTask)
+
+  useEffect(() => {
+		if (created) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "auto";
+		}
+	}, [created]);
+
+  return (
+    <div className="w-[calc(100%-2rem)] max-w-[1300px] flex-1 min-h-[300px]">
+        <CreateTaskSection 
+          setCreated={setCreated} setCreatedTask={setCreatedTask}
+        />
+        <TasksList reFetching={reFetching} setRefetching={setRefetching} />
+
+        {created && (
+          <CreatedTaskCard
+            task={createdTask}
+            setCreated={setCreated}
+            setRefetching={setRefetching}
+          />
+			)}
+    </div>
+  )
+}
+
+export default Tasks
