fd227cc235ca53e12c0a6eb0e80c6ea39ead9d99
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-01 18:57:54 +0530

    added delete and get task function

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -51,3 +51,34 @@ export const getTasks = async (request, response) => {
 			.send({ message: err.message || "Something went wrong" });
 	}
 };
+
+export const deleteTask = async (request, response) => {
+	try {
+		const { id } = request.params;
+
+		const taskToDelete = await Task.findByIdAndDelete(id);
+		if (!taskToDelete) {
+			return response.status(404).json({ message: "Task to delete not found" });
+		}
+		return response.status(200).json({ message: "Task deleted successfully" });
+	} catch (err) {
+		console.log(err.message);
+		return response
+			.status(500)
+			.send({ message: err.message || "Something went wrong" });
+	}
+};
+
+export const getTask = async (request, response) => {
+	try {
+		const { id } = request.params;
+
+		const task = await Task.findById(id);
+		return response.status(200).json(task);
+	} catch (err) {
+		console.log(err.message);
+		return response
+			.status(500)
+			.send({ message: err.message || "Something went wrong" });
+	}
+};
--- server/routes/tasksRoutes.js
+++ server/routes/tasksRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import {
-	createTask
+	createTask,
+	getTasks,
+	getTask,
+	deleteTask
 } from "../controllers/tasksController.js";
 
 const router = express.Router();
 
 router.post("/", createTask);
+router.get("/", getTasks);
+router.get("/:id", getTask);
+router.delete("/:id", deleteTask);
 
 export default router;
