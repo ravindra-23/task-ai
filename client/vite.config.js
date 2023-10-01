aa5680ca7e0ec846f95428d91f64365ce48160c8
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-01 20:16:45 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.jsx',
-    },
-  },
 })
