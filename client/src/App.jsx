6abf234758b3af74235e3ba3dc3bde27b1a6321c
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-10-01 19:30:13 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
+import Navbar from "./scenes/global/Navbar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./scenes/pages/Home"
+import Footer from "./scenes/global/Footer"
+import Tasks from "./scenes/pages/Tasks"
+
 function App() {
 
   return (
-    <div>
-      react app
-    </div>
+    <main className="w-full max-w-[2000px] min-h-screen m-auto flex flex-col items-center relative">
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </main>
   )
 }
 
