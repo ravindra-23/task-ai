a0e15a28571a8c55c29e6d6c309b85e50ecc3dab
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-12-04 17:02:44 +0530

    refactor

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { AiOutlineSave } from "react-icons/ai";
 import { FiEdit } from "react-icons/fi";
 import axios from "axios";
-import Loader from './Loader';
+import Loader from "./Loader";
 
 const TaskItem = ({ task, setRefetching, setViewEditModel, setTaskId }) => {
   const [isDeleting, setIsDeleting] = useState(false);
@@ -13,110 +13,109 @@ const TaskItem = ({ task, setRefetching, setViewEditModel, setTaskId }) => {
 
   const handleDeleteTask = async () => {
     try {
-        setIsDeleting(true);
-        await axios.delete(
-            `https://task-ai.onrender.com/${task._id}`,
-        );
-        setIsDeleting(false);
-        setRefetching((ref) => !ref);
+      setIsDeleting(true);
+      await axios.delete(`https://task-ai.onrender.com/tasks/${task._id}`);
+      setIsDeleting(false);
+      setRefetching((ref) => !ref);
     } catch (err) {
-        setIsDeleting(false);
-        console.log(err);
+      setIsDeleting(false);
+      console.log(err);
     }
-  }
+  };
   return (
-    <div className={`rounded-lg backdrop-blur-xl border border-brand p-4 relative flex flex-col ${isDeleting && "opacity-50"}`}
-        onPointerLeave={() => setViewMenu(false)}
+    <div
+      className={`rounded-lg backdrop-blur-xl border border-brand p-4 relative flex flex-col ${
+        isDeleting && "opacity-50"
+      }`}
+      onPointerLeave={() => setViewMenu(false)}
     >
-        <div className='flex gap-2 items-center justify-between mb-2 relative'>
-            <div className="flex gap-2">
-                {task?.tags.map((tag) => (
-                    <span
-                        key={tag}
-                        className="bg-hovery rounded-[100px] border border-custom_01 px-2 text-tiny"
-                    >
-                        {tag}
-                    </span>
-                ))}
-            </div>
-
-            <button onClick={() => setViewMenu(!viewMenu)}>
-                <BiDotsVerticalRounded size={24} />
-            </button>
-
-            {viewMenu && (
-                <div
-                    className="absolute z-50 right-0 top-0 rounded-sm p-[1px] flex flex-col gap-0 bg-[rgba(35,31,31,0.8)] border border-hovery w-28"
-                >
-                    <button
-                        className="opacity-80 hover:opacity-100 hover:bg-hovery transition-all py-[6px] rounded-sm flex gap-2 items-center justify-center hover:text-red"
-                        onClick={handleDeleteTask}
-                    >
-                        <MdOutlineDelete size={22} className="text-red" />
-                        Delete
-                    </button>
-                    <button
-                        className="opacity-80 hover:opacity-100 hover:bg-hovery transition-all py-[6px] rounded-sm flex gap-2 items-center justify-center hover:text-brand"
-                        onClick={() => {
-                            setViewEditModel(true);
-                            setTaskId(task._id);
-                        }}
-                    >
-                        <FiEdit size={18} className="text-brand" />
-                        Edit
-                    </button>
-                </div>
-            )}
-        </div>
-
-        <p className="flex items-center gap-1 text-tiny text-tertiary mb-4">
-            <AiOutlineSave size={20} />
-            {new Date(task.updatedAt).getDate() < 10
-                ? `0${new Date(task.updatedAt).getDate()}`
-                : new Date(task.updatedAt).getDate()}
-            /
-            {new Date(task.updatedAt).getMonth() < 9
-                ? `0${new Date(task.updatedAt).getMonth() + 1}`
-                : new Date(task.updatedAt).getMonth() + 1}
-            /{new Date(task.updatedAt).getFullYear()}
-        </p>
-
-        <h1 className="text-large font-[400] font-poppins pb-1 border-b border-custom_01 capitalize">
-            {task.title}
-        </h1>
-
-        <div className="flex gap-2 items-center justify-between mt-4">
+      <div className="flex gap-2 items-center justify-between mb-2 relative">
+        <div className="flex gap-2">
+          {task?.tags.map((tag) => (
             <span
-                className={`text-tiny uppercase 
-                ${
-                    task.priority.toUpperCase() === "LOW"
-                        ? "text-orange"
-                        : task.priority.toUpperCase() === "MEDIUM"
-                        ? "text-green"
-                        : "text-red"
-                } `}
+              key={tag}
+              className="bg-hovery rounded-[100px] border border-custom_01 px-2 text-tiny"
             >
-                {task.priority}
+              {tag}
             </span>
-            <BsFillInfoCircleFill
-                size={19}
-                fill={`${
-                    task.priority.toUpperCase() === "LOW"
-                        ? "var(--orange)"
-                        : task.priority.toUpperCase() === "MEDIUM"
-                        ? "var(--green)"
-                        : "var(--red)"
-                }`}
-            />
+          ))}
         </div>
 
-        {isDeleting && (
-            <div className="absolute z-[60] top-0 left-0 bg-hovery rounded-lg w-full h-full flex items-center justify-center gap-2">
-                <Loader /> Deleting...
-            </div>
-        )}
-    </div>
-  )
-}
+        <button onClick={() => setViewMenu(!viewMenu)}>
+          <BiDotsVerticalRounded size={24} />
+        </button>
 
-export default TaskItem
+        {viewMenu && (
+          <div className="absolute z-50 right-0 top-0 rounded-sm p-[1px] flex flex-col gap-0 bg-[rgba(35,31,31,0.8)] border border-hovery w-28">
+            <button
+              className="opacity-80 hover:opacity-100 hover:bg-hovery transition-all py-[6px] rounded-sm flex gap-2 items-center justify-center hover:text-red"
+              onClick={handleDeleteTask}
+            >
+              <MdOutlineDelete size={22} className="text-red" />
+              Delete
+            </button>
+            <button
+              className="opacity-80 hover:opacity-100 hover:bg-hovery transition-all py-[6px] rounded-sm flex gap-2 items-center justify-center hover:text-brand"
+              onClick={() => {
+                setViewEditModel(true);
+                setTaskId(task._id);
+              }}
+            >
+              <FiEdit size={18} className="text-brand" />
+              Edit
+            </button>
+          </div>
+        )}
+      </div>
+
+      <p className="flex items-center gap-1 text-tiny text-tertiary mb-4">
+        <AiOutlineSave size={20} />
+        {new Date(task.updatedAt).getDate() < 10
+          ? `0${new Date(task.updatedAt).getDate()}`
+          : new Date(task.updatedAt).getDate()}
+        /
+        {new Date(task.updatedAt).getMonth() < 9
+          ? `0${new Date(task.updatedAt).getMonth() + 1}`
+          : new Date(task.updatedAt).getMonth() + 1}
+        /{new Date(task.updatedAt).getFullYear()}
+      </p>
+
+      <h1 className="text-large font-[400] font-poppins pb-1 border-b border-custom_01 capitalize">
+        {task.title}
+      </h1>
+
+      <div className="flex gap-2 items-center justify-between mt-4">
+        <span
+          className={`text-tiny uppercase 
+                ${
+                  task.priority.toUpperCase() === "LOW"
+                    ? "text-orange"
+                    : task.priority.toUpperCase() === "MEDIUM"
+                    ? "text-green"
+                    : "text-red"
+                } `}
+        >
+          {task.priority}
+        </span>
+        <BsFillInfoCircleFill
+          size={19}
+          fill={`${
+            task.priority.toUpperCase() === "LOW"
+              ? "var(--orange)"
+              : task.priority.toUpperCase() === "MEDIUM"
+              ? "var(--green)"
+              : "var(--red)"
+          }`}
+        />
+      </div>
+
+      {isDeleting && (
+        <div className="absolute z-[60] top-0 left-0 bg-hovery rounded-lg w-full h-full flex items-center justify-center gap-2">
+          <Loader /> Deleting...
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TaskItem;
--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,116 +1,109 @@
-import { Task } from '../mongodb/models/task.js';
+import { Task } from "../mongodb/models/task.js";
 
 export const createTask = async (request, response) => {
-	try {
-		const {
-			body: { title, id, description, category, priority, tags, notes }
-		} = request;
+  try {
+    const {
+      body: { title, id, description, category, priority, tags, notes },
+    } = request;
 
-		if (
-			!title ||
-			!id ||
-			!description ||
-			!category ||
-			!priority ||
-			!tags ||
-			!notes
-		) {
-			return response.status(400).send({ message: "Missing required data" });
-		}
+    if (
+      !title ||
+      !id ||
+      !description ||
+      !category ||
+      !priority ||
+      !tags ||
+      !notes
+    ) {
+      return response.status(400).send({ message: "Missing required data" });
+    }
 
-		const newTask = {
-			title,
-			id,
-			description,
-			category,
-			priority,
-			tags,
-			notes,
-		};
-		const task = await Task.create(newTask);
-		return response.status(201).send(task);
-	} catch (err) {
-		console.log(err.message);
-		return response
-			.status(500)
-			.send({ message: err.message || "Something went wrong" });
-	}
+    const newTask = {
+      title,
+      id,
+      description,
+      category,
+      priority,
+      tags,
+      notes,
+    };
+    const task = await Task.create(newTask);
+    return response.status(201).send(task);
+  } catch (err) {
+    console.log(err.message);
+    return response
+      .status(500)
+      .send({ message: err.message || "Something went wrong" });
+  }
 };
 
 export const getTasks = async (request, response) => {
-	try {
-		const tasks = await Task.find({});
-		return response.status(200).json({
-			count: tasks.length,
-			data: tasks,
-		});
-	} catch (err) {
-		console.log(err.message);
-		return response
-			.status(500)
-			.send({ message: err.message || "Something went wrong" });
-	}
+  try {
+    const tasks = await Task.find({});
+    return response.status(200).json({
+      count: tasks.length,
+      data: tasks,
+    });
+  } catch (err) {
+    console.log(err.message);
+    return response
+      .status(500)
+      .send({ message: err.message || "Something went wrong" });
+  }
 };
 
 export const deleteTask = async (request, response) => {
-	try {
-		const { id } = request.params;
+  try {
+    const { id } = request.params;
 
-		const taskToDelete = await Task.findByIdAndDelete(id);
-		if (!taskToDelete) {
-			return response.status(404).json({ message: "Task to delete not found" });
-		}
-		return response.status(200).json({ message: "Task deleted successfully" });
-	} catch (err) {
-		console.log(err.message);
-		return response
-			.status(500)
-			.send({ message: err.message || "Something went wrong" });
-	}
+    const taskToDelete = await Task.findByIdAndDelete(id);
+    if (!taskToDelete) {
+      return response.status(404).json({ message: "Task to delete not found" });
+    }
+    return response.status(200).json({ message: "Task deleted successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return response
+      .status(500)
+      .send({ message: err.message || "Something went wrong" });
+  }
 };
 
 export const getTask = async (request, response) => {
-	try {
-		const { id } = request.params;
+  try {
+    const { id } = request.params;
 
-		const task = await Task.findById(id);
-		return response.status(200).json(task);
-	} catch (err) {
-		console.log(err.message);
-		return response
-			.status(500)
-			.send({ message: err.message || "Something went wrong" });
-	}
+    const task = await Task.findById(id);
+    return response.status(200).json(task);
+  } catch (err) {
+    console.log(err.message);
+    return response
+      .status(500)
+      .send({ message: err.message || "Something went wrong" });
+  }
 };
 
 export const editTask = async (request, response) => {
-	try {
-		const {
-			body: { title, description, category, priority, tags, notes },
-			params: { id },
-		} = request;
+  try {
+    const {
+      body: { title, description, category, priority, tags, notes },
+      params: { id },
+    } = request;
 
-		if (
-			!title ||
-			!description ||
-			!category ||
-			!priority ||
-			!tags ||
-			!notes
-		) {
-			return response.status(400).send({ message: "Missing required data" });
-		}
+    if (!title || !description || !category || !priority || !tags || !notes) {
+      return response.status(400).send({ message: "Missing required data" });
+    }
 
-		const updatedTask = await Task.findByIdAndUpdate(id, request.body);
-		if (!updatedTask) {
-			return response.status(404).json({ message: "Task to update not found" });
-		}
+    const updatedTask = await Task.findByIdAndUpdate(id, request.body);
+    if (!updatedTask) {
+      return response.status(404).json({ message: "Task to update not found" });
+    }
 
-		return response.status(200).send({ message: "Task updated successfully" });
-	} catch (err) {
-		console.log(err.message);
-		return response
-			.status(500)
-			.send({ message: err.message || "Something went wrong" });
-	}
+    return response.status(200).send({ message: "Task updated successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return response
+      .status(500)
+      .send({ message: err.message || "Something went wrong" });
+  }
 };
